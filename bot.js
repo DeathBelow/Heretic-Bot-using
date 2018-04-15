34ffb4616f39104f8db2b95400ac2a8c10eeeb68
DeathBelow <minekind22@outlook.com>
2018-04-15 21:30:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('read', () => {
-	console.log('I am ready!');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on('message',message => {
-	if (message.content === 'ping') {
-		mesage.reply('pong');
-		}
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-// THIS MUST BE THIS WAY
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
