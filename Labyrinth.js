1c79697842e269d79cd4a020fa771d9ad2963c4c
DeathBelow <minekind22@outlook.com>
2018-04-16 00:23:30 +0200

    Update Labyrinth.js

--- Labyrinth.js
+++ Labyrinth.js
@@ -1,10 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const botconfig = require("./Botconfig.json");
-const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-let xp = require("./xp.json");
-bot.commands = new Discord.Collection();
 
 client.on('ready', () => {
     console.log('Alive.');
